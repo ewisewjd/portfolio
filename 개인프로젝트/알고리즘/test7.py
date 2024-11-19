7f8f538e6d5e48fecb5056e2c5e3678252b32ae1
Your Name <kmseok4@naver.com>
2024-11-19 14:50:04 +0900

    please

---
+++ 개인프로젝트/알고리즘/test7.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+t=int(input())
+for _ in range(t):
+    N,M=map(int,input().split())
+    Matrix=[[0]*N for _ in range(N)]
+    
+    for i in range(M):
+        u,v,c=map(int,input().split())
+        Matrix[u][v]=c
+        
+        
+    for i in range(N):
+        print(*Matrix[i])
+
